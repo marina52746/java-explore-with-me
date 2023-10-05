27171b1d1e103d90609a95f2023829ea65c9b2a4
Marina <marina52746@yandex.ru>
2023-10-05 19:11:11 +0300

    fix: changes in pom - add spring-boot-maven-plugin

--- main-service/src/main/java/ru/practicum/Main.java
+++ main-service/src/main/java/ru/practicum/Main.java
@@ -1,10 +1,16 @@
 package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /*
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
+*/
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) { */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         /*
         user
         category
@@ -20,7 +26,6 @@ public class Main {
         PrintWriter printWriter = new PrintWriter(stringWriter);
         ex.printStackTrace(printWriter);
         String error = stringWriter.toString();
-        System.out.println(error);
+        System.out.println(error); */
     }
 }
- */
