365f987157395b429afe75f307764a5e5b16e282
Marina <marina52746@yandex.ru>
2023-10-05 19:23:59 +0300

    fix: checkstyle - removed comments

--- main-service/src/main/java/ru/practicum/user/UserController.java
+++ main-service/src/main/java/ru/practicum/user/UserController.java
@@ -5,11 +5,5 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
     UserService userService;
-/*
-    @GetMapping
-    List<User> findBy(String param1, String param2, String param3, String param4, String param5, String param6) {
-        UserParam param = new UserParam(param1, param2, param3, param4, param5, param6);
-        return UserService.findBy(param);
-    }
- */
+
 }
--- main-service/src/main/java/ru/practicum/user/UserService.java
+++ main-service/src/main/java/ru/practicum/user/UserService.java
@@ -4,12 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    /*
-    UserRepository userRepository;
 
-    public List<User> findBy(UserParam param, Pageable pageable) {
-        Specification<User> spec = new UserSpecification(param); // часть для where на java коде
-        return userRepository.findBy(spec, pageable);
-    }
-     */
 }
